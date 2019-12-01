6874874da194660a256c555670bfb1a372eb3511
Maha Ashour <mash@bu.edu>
2019-12-01 02:17:10 -0500

    improved logging in build server main script

--- kvm-xfstests/test-appliance/files/usr/local/lib/gce-bldsrv/bldsrv.py
+++ kvm-xfstests/test-appliance/files/usr/local/lib/gce-bldsrv/bldsrv.py
@@ -6,6 +6,11 @@ class BLDSRV(object):
   server_log_file = '/var/log/bldsrv/bldsrv.log'
   build_log_dir = '/var/log/bldsrv/bldsrv_logs/'
   bldsrv_username = 'bldsrv'
+  image_path = '/arch/x86/boot/bzImage'
+  image_name = 'bzImage'
+  repo_cache_path = '/repositories/'
+  gce_update_repo = '/usr/local/lib/gce-update-repo'
+  gce_build_kernel = '/usr/local/lib/gce-build-kernel'
 
   @staticmethod
   def create_log_dir(log_file_path):
